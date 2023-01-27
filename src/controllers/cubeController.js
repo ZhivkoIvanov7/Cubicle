eb7edb61b8fcf805e97c390e73467fe3b1b5dad5
ZhivkoIvanov7 <107200820+ZhivkoIvanov7@users.noreply.github.com>
2023-01-27 13:05:33 +0200

    Save cube to db

--- src/controllers/cubeController.js
+++ src/controllers/cubeController.js
@@ -1,28 +1,29 @@
-const Cube = require('../models/Cube_Old');
+const Cube = require('../models/Cube');
 const db = require('../db.json');
 
 exports.getCreateCube = (req, res) => {
     res.render('create');
 };
 
-exports.postCreateCube = (req, res) => {
-    let cube = new Cube(...Object.values(req.body));
-    cube.save();
-    
+exports.postCreateCube = async (req, res) => {
+    const { name, description, imageUrl, difficultyLevel } = req.body;
+    let cube = new Cube({ name, description, imageUrl, difficultyLevel });
+    await cube.save();
+
     res.redirect('/');
 };
 
 exports.getDetails = (req, res) => {
     let cubeId = Number(req.params.cubeId);
-    if(!cubeId) {
+    if (!cubeId) {
         return res.redirect('/404');
     }
 
     let cube = db.cubes.find(x => x.id === cubeId);
 
-    if(!cube){
+    if (!cube) {
         return res.redirect('/404');
     }
 
-    res.render('details', {cube});
+    res.render('details', { cube });
 }
