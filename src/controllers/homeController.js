4a018f834453453c2d2b4d2c9abe45ac049be60f
ZhivkoIvanov7 <107200820+ZhivkoIvanov7@users.noreply.github.com>
2023-01-27 13:14:42 +0200

    Show cubes from db in home page

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,8 +1,9 @@
-const db = require('../db.json');
+const Cube = require('../models/Cube');
 
-exports.getHomePage = (req, res) => {
+exports.getHomePage = async (req, res) => {
     const { search, from: difficultyFrom, to: difficultyTo } = req.query;
-    let cubes = db.cubes;
+
+    let cubes = await Cube.find().lean();
 
     if (search) {
         cubes = cubes.filter(cube => cube.name.toLowerCase().includes(search.toLowerCase()));
@@ -16,7 +17,7 @@ exports.getHomePage = (req, res) => {
         cubes = cubes.filter(cube => cube.difficultyLevel <= difficultyTo);
     }
 
-    res.render('index', { cubes });
+    res.render('index', { cubes, search, difficultyFrom, difficultyTo });
 };
 
 exports.getAboutPage = (req, res) => {
